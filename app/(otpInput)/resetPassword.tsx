88a4db23e2a8597df411c1df0d49443723834f5c
Dhana lashmi <dhanalakshmi763927@gmail.com>
2025-11-13 12:34:32 +0530

    resetPassword

--- app/(otpInput)/resetPassword.tsx
+++ app/(otpInput)/resetPassword.tsx
@@ -2,11 +2,51 @@ import CustomButton from "@/components/ui/Button";
 import Input from "@/components/ui/inputField";
 import { Typography } from "@/components/ui/Typography";
 import { Ionicons } from "@expo/vector-icons";
+import { useLocalSearchParams } from "expo-router";
 import { push } from "expo-router/build/global-state/routing";
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, View } from "react-native";
 
 export default function ResetPassword() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword]=useState('')
+  const {email} = useLocalSearchParams()
+
+  const handleResetPassword = async()=>{
+    if (!password || !confirmPassword) {
+      Alert.alert("Error", "please fill in both fields")
+      return
+    }
+    if (password !==confirmPassword) {
+      Alert.alert("Error", "Passwords do not match")
+      return
+    }
+    try {
+      const response = await fetch("http://localhost:5000/api/user/reset-password",{
+        method: "POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          email,
+          newPassword: password
+        })
+      })
+
+      const data = await response.json()
+      if (response.ok) {
+        Alert.alert("Success", data.message)
+        push("/(otpInput)/passwordChanged")
+      } else {
+        Alert.alert("Error",data.message|| "something went wrong")
+      }
+    } catch (error) {
+      console.log("Reset password error:", error);
+      Alert.alert("Error", "Failed to connect to server")
+      
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -42,6 +82,8 @@ export default function ResetPassword() {
         placeholder="must be 8 characters"
         autoCapitalize="none"
         keyboardType="email-address"
+        value={password}
+        onChangeText={(password:any)=>setPassword(password)}
         secureTextEntry
         rightIcon={<Ionicons name="eye" size={20} color="#6A0066"/>}
       />
@@ -51,6 +93,8 @@ export default function ResetPassword() {
         placeholder="repeat password"
         autoCapitalize="none"
         keyboardType="email-address"
+        value={confirmPassword}
+        onChangeText={(confirmPassword:any)=>setConfirmPassword(confirmPassword)}
         secureTextEntry
         rightIcon={<Ionicons name="eye" size={20} color="#6A0066"/>}
       />
@@ -61,7 +105,7 @@ export default function ResetPassword() {
         title="Reset password"
         variant="primary"
         size="large"
-        onPress={() => push("/(otpInput)/passwordChanged")}
+        onPress={handleResetPassword}
       ></CustomButton>
       <Typography
         variant="regular"
